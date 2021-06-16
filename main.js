025b8087276e4dea0f205040c1bf6913a57e1b2c
suhanijain2212 <75556928+suhanijain2212@users.noreply.github.com>
2021-06-16 09:43:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+Webcam.set({
+    width: 310,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90,
+    
+    constraints: {
+        facingMode: "environment"
+    }
+    });
+    camera = document.getElementById("camera");
+    
+    Webcam.attach('#camera');
+    
+    function take_snapshot()
+    {
+        Webcam.snap(function(data_uri) {
+            document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        });
+    }
+    
+    console.log('ml5 version;', ml5.version);
+    classifier=ml5.ImageClassifier('MobileNet', modelLoaded);
+    
+    function modelLoaded()
+    {
+        console.log("modelLoaded");
+    }
+    
+    function check()
+    {
+        img= document.getElementById('captured_image');
+        classifier.classify(img, gotResult);
+    }
+    
+    function gotResult(error, results) {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log(results);
+            document.getElementById("onject_name").innerHTML = results[0].label;
+        }
+    }
+    
